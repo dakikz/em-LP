ca1e5cfb0b107f21ca2b7ebbd1850e27965c9d34
dakikz <petrosleonidou@outlook.com>
2017-07-31 16:18:18 +0300

    Add files via upload

---
+++ EasyMarketsLP/assets/js/registration.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    $('#contactForm').bootstrapValidator({
+        container: '#messages',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            firstName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The first name is required and cannot be empty'
+                    }
+                }
+            },
+            lastName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The last name is required and cannot be empty'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email address is required and cannot be empty'
+                    },
+                    emailAddress: {
+                        message: 'The email address is not valid'
+                    }
+                }
+            },
+            phone: {
+            	validators: {
+                    notEmpty: {
+                        message: 'The phone is required and cannot be empty'
+                    },
+                    phone: {
+                        message: 'The phone is not valid'
+                    }
+                }
+            },
+        }
+    });
+});
